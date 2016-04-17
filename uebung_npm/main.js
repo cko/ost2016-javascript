0903b13d1cb6df66dd9cb7c353369e9e8264885c
Christine Koppelt <cko@users.noreply.github.com>
2016-04-17 15:55:36 +0200

    add slides part 2 and npm exercise

---
+++ uebung_npm/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let http = require('http');
+
+let server = http.createServer( (request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+server.listen(8888);
+
+console.log("Server running at http://127.0.0.1:8888/");
+
